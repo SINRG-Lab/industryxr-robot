3acf56db4e71f50ff94f1bd348875c89e5490742
mayankchadha28 <mayankchadha1998@gmail.com>
2024-10-15 01:45:54 -0400

    singleton BoardManager

--- src/sinrg_robot_sdk/sinrg_robot_sdk/robot_board_manager.py
+++ src/sinrg_robot_sdk/sinrg_robot_sdk/robot_board_manager.py
@@ -9,7 +9,8 @@ from sinrg_robot_sdk.robot_controller_sdk import Board
 class BoardManager:
     
     _instance = None
-    _boardStatus = None
+    _isBoardActive = None
+    _board = None
 
     # def __new__(cls):
     #     if cls._instance is None:
@@ -17,50 +18,48 @@ class BoardManager:
     #     return cls._instance
 
     def __init__(self):
-        
-        # self.isBoardActive = False
         # self.configUtil = ConfigUtil() 
         # self.uartAddress = self.configUtil.getValue("communication", "UART_ADDRESS")
         # if hasattr(self, 'Initialized'):
-        if BoardManager._boardStatus is None:
+        if BoardManager._isBoardActive is None:
             self.uartAddress = "/dev/ttyACM0"
+            self.setBoard()
             # self.setBoard()
-            self.board = Board(device= self.uartAddress)
-            self._activateBoard()
-            BoardManager._boardStatus = "active"
+            # self.board = Board(device= self.uartAddress)
+            # self._activateBoard()
+            # BoardManager._isBoardActive = True
             print("Board is set to Active state")
+
         else:
             print("Board is already active. using prev instance")
+
+    def setBoard(self):
+        BoardManager._board = Board(device= self.uartAddress)
+        self._activateBoard()
+        BoardManager._isBoardActive = True
         
     def getBoard(self):
-        return self.board
-    
-    def setBoardStatus(self):
-        self._boardStatus = True
-
-    def getBoardStatus(self):
-        return self._boardStatus
-    
-    # def setBoard(self):
-    #     self.board = Board(device= self.uartAddress)
-    #     self._boardStatus = True
+        if BoardManager._board is not None:
+            return BoardManager._board
+        else:
+            return -1
 
     def _activateBoard(self):
-        self.board.enable_reception()
+        BoardManager._board.enable_reception()
         logging.info("Robot Controller Initialized.")
 
-    def getBoardBattery(self):
-        return self.board.get_battery()
+    # def getBoardBattery(self):
+    #     return self.board.get_battery()
     
-    def setBoardLed(onTime: int, offTime: int, cycleCount: int):
-        pass
+    # def setBoardLed(onTime: int, offTime: int, cycleCount: int):
+    #     pass
 
-    def setTimestamp(self):
-        self.timestamp = str(datetime.now(timezone.utc).isoformat())
-        return self.timestamp
+    # def setTimestamp(self):
+    #     self.timestamp = str(datetime.now(timezone.utc).isoformat())
+    #     return self.timestamp
 
-    def getTimestamp(self):
-        return self.timestamp
+    # def getTimestamp(self):
+    #     return self.timestamp
 
     # def __new__(cls):
     #     if cls._instance is None:
